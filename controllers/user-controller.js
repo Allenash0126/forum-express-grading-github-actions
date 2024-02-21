aa6281a69930649fee58a362f9bd22929954f48f
allenash0126 <allen64031@hotmail.com>
2024-02-21 15:56:03 +0800

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,13 +6,23 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    const { name, email, password } = req.body
-    bcrypt.hash(password, 10)
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
+    if (password !== passwordCheck) throw new Error(' Passwords do not match')
+    User.findOne({
+      where: { email }
+    })
+      .then(user => {
+        if (user) throw new Error(' Email already exists!')
+        return bcrypt.hash(password, 10)
+      })
+
       .then(hash => User.create({ name, email, password: hash }))
       .then(() => {
+        req.flash('success', '註冊成功！')
         res.redirect('/signin')
       })
+      .catch(err => next(err))
   }
 }
 
