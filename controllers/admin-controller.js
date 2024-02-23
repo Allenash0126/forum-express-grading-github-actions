833ac518492a0ae67c5666d5af0fbe3072b21497
allenash0126 <allen64031@hotmail.com>
2024-02-23 19:04:08 +0800

    feat: add restaurant image

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const { Restaurant } = require('../models')
+const { localFileHandler } = require('../helpers/file-helpers')
 // 解構賦值如下
 // const db = require('../models')
 // const Restaurant = db.Restaurant
@@ -18,7 +19,10 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
 
-    Restaurant.create({ name, tel, address, openingHours, description })
+    const { file } = req
+
+    localFileHandler(file)
+      .then(filePath => Restaurant.create({ name, tel, address, openingHours, description, image: filePath || null }))
       .then(() => {
         req.flash('success', 'Restaurant was created successfully!')
         res.redirect('/admin/restaurants')
@@ -50,15 +54,22 @@ const adminController = {
   putRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
-    Restaurant.findByPk(req.params.rest_id)
-      .then(restaurant => {
+
+    const { file } = req
+
+    Promise.all([
+      Restaurant.findByPk(req.params.rest_id),
+      localFileHandler(file)
+    ])
+      .then(([restaurant, filePath]) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        restaurant.update({
+        return restaurant.update({
           name,
           tel,
           address,
           openingHours,
-          description
+          description,
+          image: filePath || restaurant.image
         })
       })
       .then(() => {
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const upload = require('../../middleware/multer')
 
 router.get('/restaurants/create', adminController.createRestaurants)
 router.get('/restaurants/:rest_id/edit', adminController.editRestaurant)
 router.get('/restaurants/:rest_id', adminController.getRestaurant)
-router.put('/restaurants/:rest_id', adminController.putRestaurant)
+router.put('/restaurants/:rest_id', upload.single('image'), adminController.putRestaurant)
 router.delete('/restaurants/:rest_id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
-router.post('/restaurants', adminController.postRestaurants)
+router.post('/restaurants', upload.single('image'), adminController.postRestaurants)
 
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
