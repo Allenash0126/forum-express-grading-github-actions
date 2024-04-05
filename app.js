20df4373eb79aec917a965a7c02c1f8ce8d5c7a2
allenash0126 <allen64031@hotmail.com>
2024-04-05 22:52:29 +0800

    feat: api signin add jwt token

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const { getUser } = require('./helpers/auth-helpers')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
 const { pages, apis } = require('./routes')
+if (process.env.NODE_ENV === 'development') {
+	require('dotenv').config()
+}
 
 const app = express()
 const port = process.env.PORT || 3000
